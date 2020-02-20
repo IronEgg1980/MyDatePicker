dab01259b2b7a925d6b96b7e219a4181cff6de7d
IronEgg1980 <yinzongwang@163.com>
2020-02-21 00:31:08 +0800

    20200221

--- app/src/main/java/home/yzw/mydatepicker/MainActivity.java
+++ app/src/main/java/home/yzw/mydatepicker/MainActivity.java
@@ -3,29 +3,41 @@ package home.yzw.mydatepicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
-
+    private SimpleDateFormat format;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        format =new SimpleDateFormat("yyyy年M月d日 EEEE",Locale.CHINA);
         textView = findViewById(R.id.textview);
-        MyDatePicker datePicker = findViewById(R.id.datepicker);
+        final MyDatePicker datePicker = findViewById(R.id.datepicker);
+        datePicker.isMultiSelect = true;
         datePicker.setClickListener(new MyDatePicker.DatePickerClickListener() {
             @Override
             public void onClick(int year, int month, int dayOfMonth) {
                 Calendar calendar = new GregorianCalendar(Locale.CHINA);
                 calendar.set(year,month,dayOfMonth);
-                textView.setText(new SimpleDateFormat("yyyy年M月d日 EEEE",Locale.CHINA).format(calendar.getTime()));
+                textView.setText(format.format(calendar.getTime()));
+            }
+        });
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long[] times = datePicker.getSelectDateRange();
+                String s = format.format(times[0]) +" | "+format.format(times[1]);
+                textView.setText(s);
             }
         });
     }
--- app/src/main/java/home/yzw/mydatepicker/MyDatePicker.java
+++ app/src/main/java/home/yzw/mydatepicker/MyDatePicker.java
@@ -19,104 +19,93 @@ import java.util.Locale;
 import androidx.annotation.Nullable;
 
 public class MyDatePicker extends LinearLayout {
-
-    public interface DatePickerClickListener{
-        void onClick(int year,int month,int dayOfMonth);
+    public interface DatePickerClickListener {
+        void onClick(int year, int month, int dayOfMonth);
     }
 
     public void setClickListener(DatePickerClickListener clickListener) {
         this.clickListener = clickListener;
     }
 
-    private String TAG = "殷宗旺";
-
     private DatePickerClickListener clickListener;
-
-    private boolean isMultiSelect = false;
-
-    private int dateTextNormalColor,dateTextSelectedColor,dateNormalBG,dateSelectedBG;
+    public boolean isMultiSelect = false;
+    private long[] selectDateRange;
+    private boolean isFirstSelect = true;
+    private int dateTextNormalColor, dateTextSelectedColor, dateNormalBG, dateSelectedBG;
     private Drawable selectedBGDrawable;
     private Calendar mCalendar;
-    private SimpleDateFormat dateFormat,dayOfMonthFormat;
-    private long today,selectedDay,monthDay;
+    private SimpleDateFormat dateFormat, dayOfMonthFormat;
+    private long today, selectedDay, monthDay;
     private int[] ids;
-    private int currentId;
-    private TextView preMonthView, titleView,nextMonthView;
+    private int currentIndex;
+    private TextView preMonthView, titleView, nextMonthView;
     private LinearLayout dateViewGroup;
 
     public MyDatePicker(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.MyDatePicker);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MyDatePicker);
         dateTextNormalColor = typedArray.getColor(R.styleable.MyDatePicker_dateTextNormalColor, Color.BLACK);
-        dateTextSelectedColor = typedArray.getColor(R.styleable.MyDatePicker_dateTextSelectedColor,Color.WHITE);
-        dateNormalBG = typedArray.getColor(R.styleable.MyDatePicker_dateNormalBG,Color.TRANSPARENT);
-        dateSelectedBG = typedArray.getColor(R.styleable.MyDatePicker_dateSelectedBG,Color.parseColor("#0e7772"));
+        dateTextSelectedColor = typedArray.getColor(R.styleable.MyDatePicker_dateTextSelectedColor, Color.WHITE);
+        dateNormalBG = typedArray.getColor(R.styleable.MyDatePicker_dateNormalBG, Color.TRANSPARENT);
+        dateSelectedBG = typedArray.getColor(R.styleable.MyDatePicker_dateSelectedBG, Color.parseColor("#0e7772"));
         selectedBGDrawable = typedArray.getDrawable(R.styleable.MyDatePicker_dateSelectedDrawable);
         typedArray.recycle();
         initial();
         initialViews();
-        setDate(today);
+        setSelectedDate(today);
     }
 
-    public void setDate(long timeInMillis){
-        long[] values =new long[42];
+    public void setSelectedDate(long timeInMillis) {
+        if(isMultiSelect)
+            return;
+        selectedDay = timeInMillis;
+        setDateView(timeInMillis);
+    }
+
+    public long[] getSelectDateRange() {
+        return selectDateRange;
+    }
+
+    private void setDateView(long timeInMillis) {
         mCalendar.setTimeInMillis(timeInMillis);
-        mCalendar.set(Calendar.DAY_OF_MONTH,15);
+        mCalendar.set(Calendar.DAY_OF_MONTH, 15);
         monthDay = mCalendar.getTimeInMillis();
 
-        mCalendar.set(Calendar.DAY_OF_MONTH,1);
+        mCalendar.set(Calendar.DAY_OF_MONTH, 1);
         int week = mCalendar.get(Calendar.DAY_OF_WEEK);
-        int diff = week> 1 && week< 4? (-5-week) % 7 - 7:  (-5-week) % 7;
-        mCalendar.add(Calendar.DAY_OF_MONTH,diff);
+        int diff = week > 1 && week < 4 ? (-5 - week) % 7 - 7 : (-5 - week) % 7;
+        mCalendar.add(Calendar.DAY_OF_MONTH, diff);
 
-        for(int i  = 0;i<42;i++){
-            if(i > 0)
-                mCalendar.add(Calendar.DAY_OF_MONTH,1);
-            values[i] = mCalendar.getTimeInMillis();
-        }
-
-        for(int i = 0;i<42;i++){
-            long time = values[i];
-            String dateString = dayOfMonthFormat.format(time);
+        for (int i = 0; i < 42; i++) {
             TextView textView = findViewById(ids[i]);
-            if(textView!=null){
-                textView.setTextColor(dateTextNormalColor);
-                textView.setBackgroundColor(dateNormalBG);
-                textView.setText(dateString);
-                textView.setTag(time);
-                if (compareDay(time, selectedDay) == 0) {
-                    textView.setTextColor(dateTextSelectedColor);
-                    if(selectedBGDrawable != null){
-                        textView.setBackground(selectedBGDrawable);
-                    }else {
-                        textView.setBackgroundColor(dateSelectedBG);
-                    }
-                    currentId = textView.getId();
-                } else if(compareMonth(time,monthDay) != 0) {
-                    textView.setTextColor(Color.GRAY);
-                }
-            }
+            if (i > 0)
+                mCalendar.add(Calendar.DAY_OF_MONTH, 1);
+            textView.setTag(mCalendar.getTimeInMillis());
         }
         mCalendar.setTimeInMillis(monthDay);
         titleView.setText(dateFormat.format(monthDay));
+        changeDateViewUI();
     }
 
-    private void initial(){
+    private void initial() {
         mCalendar = Calendar.getInstance();
         dateFormat = new SimpleDateFormat("yyyy年M月", Locale.CHINA);
         dayOfMonthFormat = new SimpleDateFormat("d", Locale.CHINA);
         today = mCalendar.getTimeInMillis();
         selectedDay = today;
-        mCalendar.set(Calendar.DAY_OF_MONTH,15);
+        mCalendar.set(Calendar.DAY_OF_MONTH, 15);
         monthDay = mCalendar.getTimeInMillis();
-        currentId = -1;
+        currentIndex = -1;
+        selectDateRange = new long[2];
+        selectDateRange[0] = today;
+        selectDateRange[1] = today;
         ids = new int[42];
-        for(int i = 0;i<42;i++){
+        for (int i = 0; i < 42; i++) {
             ids[i] = generateViewId();
         }
     }
 
-    private void initialViews(){
+    private void initialViews() {
         setOrientation(VERTICAL);
         removeAllViews();
         addTitleViews();
@@ -124,15 +113,15 @@ public class MyDatePicker extends LinearLayout {
         addDateViews();
     }
 
-    private void addTitleViews(){
+    private void addTitleViews() {
         LinearLayout linearLayout = new LinearLayout(getContext());
         linearLayout.setOrientation(HORIZONTAL);
         LayoutParams lp = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        lp.setMargins(8,0,8,0);
+        lp.setMargins(8, 0, 8, 0);
 
         preMonthView = new TextView(getContext());
         preMonthView.setGravity(Gravity.CENTER);
-        preMonthView.setPadding(8,8,8,8);
+        preMonthView.setPadding(8, 8, 8, 8);
         preMonthView.setTextSize(25);
         preMonthView.setText("<");
         preMonthView.setTextColor(dateTextNormalColor);
@@ -145,7 +134,7 @@ public class MyDatePicker extends LinearLayout {
 
         titleView = new TextView(getContext());
         titleView.setGravity(Gravity.CENTER);
-        titleView.setPadding(8,8,8,8);
+        titleView.setPadding(8, 8, 8, 8);
         titleView.setTextSize(25);
         titleView.setText("");
         titleView.setTextColor(dateTextNormalColor);
@@ -158,7 +147,7 @@ public class MyDatePicker extends LinearLayout {
 
         nextMonthView = new TextView(getContext());
         nextMonthView.setGravity(Gravity.CENTER);
-        nextMonthView.setPadding(8,8,8,8);
+        nextMonthView.setPadding(8, 8, 8, 8);
         nextMonthView.setTextSize(25);
         nextMonthView.setText(">");
         nextMonthView.setTextColor(dateTextNormalColor);
@@ -169,77 +158,145 @@ public class MyDatePicker extends LinearLayout {
             }
         });
 
-        LayoutParams childLP = new LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,1);
-        linearLayout.addView(preMonthView,childLP);
-        linearLayout.addView(titleView,new LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,2));
-        linearLayout.addView(nextMonthView,childLP);
+        LayoutParams childLP = new LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+        linearLayout.addView(preMonthView, childLP);
+        linearLayout.addView(titleView, new LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 2));
+        linearLayout.addView(nextMonthView, childLP);
 
-        addView(linearLayout,lp);
+        addView(linearLayout, lp);
     }
 
-    private void addWeekTitleViews(){
+    private void addWeekTitleViews() {
         LinearLayout linearLayout = new LinearLayout(getContext());
         LayoutParams lp = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        lp.setMargins(8,0,8,0);
-        String[] weeks = {"一","二","三","四","五","六","日"};
-        LayoutParams childLP = new LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,1);
-        for(int i = 0 ;i<7;i++){
+        lp.setMargins(8, 0, 8, 0);
+        String[] weeks = {"一", "二", "三", "四", "五", "六", "日"};
+        LayoutParams childLP = new LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+        for (int i = 0; i < 7; i++) {
             TextView textView = new TextView(getContext());
             textView.setGravity(Gravity.CENTER);
             textView.setTextSize(20);
             textView.setText(weeks[i]);
-            if(i == 5)
+            if (i == 5)
                 textView.setTextColor(Color.GREEN);
-            if(i == 6)
+            if (i == 6)
                 textView.setTextColor(Color.RED);
-            linearLayout.addView(textView,childLP);
+            linearLayout.addView(textView, childLP);
         }
-        addView(linearLayout,lp);
+        addView(linearLayout, lp);
     }
 
-    private void addDateViews(){
+    private void addDateViews() {
         dateViewGroup = new LinearLayout(getContext());
         dateViewGroup.setOrientation(VERTICAL);
         LayoutParams lp = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        lp.setMargins(8,0,8,0);
-        LayoutParams _lp = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,0,1);
-        LayoutParams childLP = new LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT,1);
+        lp.setMargins(8, 0, 8, 0);
+        LayoutParams _lp = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1);
+        _lp.setMargins(0,8,0,8);
+        LayoutParams childLP = new LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, 1);
         LayoutParams child_child_LP = new LayoutParams(120, 120);
         child_child_LP.gravity = Gravity.CENTER;
         int index = 0;
-        for(int i = 0;i<6;i++){
-            LinearLayout linearLayout =  new LinearLayout(getContext());
+        for (int i = 0; i < 6; i++) {
+            LinearLayout linearLayout = new LinearLayout(getContext());
             linearLayout.setOrientation(HORIZONTAL);
-            for(int j = 0;j<7;j++){
+            for (int j = 0; j < 7; j++) {
                 LinearLayout ll = new LinearLayout(getContext());
                 final TextView textView = new TextView(getContext());
-                textView.setPadding(10,10,10,10);
+                textView.setPadding(10, 10, 10, 10);
                 textView.setGravity(Gravity.CENTER);
-                textView.setTextSize(20);
+                textView.setTextSize(16);
+                final int position = index;
                 textView.setId(ids[index++]);
                 textView.setTextColor(dateTextNormalColor);
                 textView.setBackgroundColor(dateNormalBG);
                 textView.setOnClickListener(new OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        dateViewClick(textView);
+                        if (isMultiSelect) {
+                            dateViewMultiClick(position);
+                        } else {
+                            dateViewClick(position);
+                        }
                     }
                 });
-                ll.addView(textView,child_child_LP);
-                linearLayout.addView(ll,childLP);
+                ll.addView(textView, child_child_LP);
+                linearLayout.addView(ll, childLP);
             }
-            dateViewGroup.addView(linearLayout,_lp);
+            dateViewGroup.addView(linearLayout, _lp);
         }
-        addView(dateViewGroup,lp);
+        addView(dateViewGroup, lp);
     }
 
-    private void dateViewClick(TextView textView){
+    private void dateViewMultiClick(int position) {
+        TextView textView = findViewById(ids[position]);
+        long time = 0;
+        if (textView != null) {
+            time = (long) textView.getTag();
+            if (isFirstSelect || compareDay(selectDateRange[0], time) > 0) {
+                isFirstSelect = false;
+                selectDateRange[0] = time;
+                selectDateRange[1] = time;
+            } else {
+                isFirstSelect = true;
+                selectDateRange[1] = time;
+            }
+            changeDateViewUI();
+        }
+    }
+
+    private void changeDateViewUI() {
+        for (int i = 0; i < 42; i++) {
+            TextView textView = findViewById(ids[i]);
+            if (textView != null) {
+                long time = (long) textView.getTag();
+                String s = dayOfMonthFormat.format(time);
+                textView.setBackgroundColor(dateNormalBG);
+                if (compareMonth(time, monthDay) != 0) {
+                    textView.setTextColor(Color.GRAY);
+                } else {
+                    textView.setTextColor(dateTextNormalColor);
+                }
+                if (isMultiSelect) {
+                    int startDiff = compareDay(time, selectDateRange[0]);
+                    int endDiff = compareDay(time, selectDateRange[1]);
+
+                    if (startDiff >= 0 && endDiff <= 0) {
+                        if (startDiff == 0) {
+                            s = dayOfMonthFormat.format(time) + "\nStart";
+                        }
+                        if (isFirstSelect && endDiff == 0) {
+                            s = dayOfMonthFormat.format(time) + "\nEnd";
+                        }
+                        textView.setTextColor(dateTextSelectedColor);
+                        if (selectedBGDrawable != null) {
+                            textView.setBackground(selectedBGDrawable);
+                        } else {
+                            textView.setBackgroundColor(dateSelectedBG);
+                        }
+                    }
+                } else if (compareDay(time, selectedDay) == 0) {
+                    textView.setTextColor(dateTextSelectedColor);
+                    if (selectedBGDrawable != null) {
+                        textView.setBackground(selectedBGDrawable);
+                    } else {
+                        textView.setBackgroundColor(dateSelectedBG);
+                    }
+                    currentIndex = i;
+                }
+                textView.setText(s);
+            }
+        }
+    }
+
+    private void dateViewClick(int position) {
+        TextView textView = findViewById(ids[position]);
         long value = (long) textView.getTag();
         selectedDay = value;
         textView.setTextColor(dateTextSelectedColor);
-        if(selectedBGDrawable != null){
+        if (selectedBGDrawable != null) {
             textView.setBackground(selectedBGDrawable);
-        }else {
+        } else {
             textView.setBackgroundColor(dateSelectedBG);
         }
 
@@ -247,50 +304,50 @@ public class MyDatePicker extends LinearLayout {
         int year = mCalendar.get(Calendar.YEAR);
         int month = mCalendar.get(Calendar.MONTH);
         int day = mCalendar.get(Calendar.DAY_OF_MONTH);
-        int diff = compareMonth(value,monthDay);
-        if(diff == 0) {
-            if (currentId != -1) {
-                TextView oldTV = findViewById(currentId);
+        int diff = compareMonth(value, monthDay);
+        if (diff == 0) {
+            if (currentIndex >= 0 && currentIndex < 42) {
+                TextView oldTV = findViewById(ids[currentIndex]);
                 if (oldTV != null) {
                     oldTV.setTextColor(dateTextNormalColor);
                     oldTV.setBackgroundColor(dateNormalBG);
                 }
-                currentId = textView.getId();
+                currentIndex = position;
             }
-        }else if(diff > 0){
+        } else if (diff > 0) {
             nextMonthClick();
-        }else {
+        } else {
             preMonthClick();
         }
 
-        if(clickListener != null){
-            clickListener.onClick(year,month,day);
+        if (clickListener != null) {
+            clickListener.onClick(year, month, day);
         }
         mCalendar.setTimeInMillis(monthDay);
     }
 
-    private void preMonthClick(){
-        mCalendar.add(Calendar.MONTH,-1);
-        mCalendar.set(Calendar.DAY_OF_MONTH,15);
+    private void preMonthClick() {
+        mCalendar.add(Calendar.MONTH, -1);
+        mCalendar.set(Calendar.DAY_OF_MONTH, 15);
         monthDay = mCalendar.getTimeInMillis();
-        setDate(monthDay);
+        setDateView(monthDay);
     }
 
-    private void currentMonthClick(){
+    private void currentMonthClick() {
         mCalendar.setTimeInMillis(today);
-        mCalendar.set(Calendar.DAY_OF_MONTH,15);
+        mCalendar.set(Calendar.DAY_OF_MONTH, 15);
         monthDay = mCalendar.getTimeInMillis();
-        setDate(monthDay);
+        setDateView(monthDay);
     }
 
-    private void nextMonthClick(){
-        mCalendar.add(Calendar.MONTH,1);
-        mCalendar.set(Calendar.DAY_OF_MONTH,15);
+    private void nextMonthClick() {
+        mCalendar.add(Calendar.MONTH, 1);
+        mCalendar.set(Calendar.DAY_OF_MONTH, 15);
         monthDay = mCalendar.getTimeInMillis();
-        setDate(monthDay);
+        setDateView(monthDay);
     }
 
-    private int compareMonth(long d1,long d2){
+    private int compareMonth(long d1, long d2) {
         mCalendar.setTimeInMillis(d1);
         int y1 = mCalendar.get(Calendar.YEAR);
         int m1 = mCalendar.get(Calendar.MONTH);
@@ -298,21 +355,21 @@ public class MyDatePicker extends LinearLayout {
         int y2 = mCalendar.get(Calendar.YEAR);
         int m2 = mCalendar.get(Calendar.MONTH);
         mCalendar.setTimeInMillis(monthDay);
-        if(y1 == y2){
-            return Integer.compare(m1,m2);
+        if (y1 == y2) {
+            return Integer.compare(m1, m2);
         }
-        return Integer.compare(y1,y2);
+        return Integer.compare(y1, y2);
     }
 
-    private int compareDay(long d1,long d2){
-        int i = compareMonth(d1,d2);
-        if(i == 0){
+    private int compareDay(long d1, long d2) {
+        int i = compareMonth(d1, d2);
+        if (i == 0) {
             mCalendar.setTimeInMillis(d1);
             int day1 = mCalendar.get(Calendar.DAY_OF_MONTH);
             mCalendar.setTimeInMillis(d2);
             int day2 = mCalendar.get(Calendar.DAY_OF_MONTH);
             mCalendar.setTimeInMillis(monthDay);
-            return Integer.compare(day1,day2);
+            return Integer.compare(day1, day2);
         }
         return i;
     }
